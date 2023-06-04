4b57cbdff3313609480559ee2f1d3437ceb27539
Tran Hung <tranviethung912002@gmail.com>
2023-06-05 03:21:36 +0700

    .

--- main/app_main.c
+++ main/app_main.c
@@ -13,6 +13,7 @@
 #define POWER_GPIO_PIN     GPIO_NUM_18
 #define CMD_SIZE           300
 #define AT_CMD_SUCCESS_BIT (1 << 0)
+#define AT_CMD_FAILURE_BIT (1 << 1)
 
 #define MQTT_SERVER "mqtt.innoway.vn"
 #define PORT        "1883"
@@ -25,35 +26,31 @@
     "d,\"longitude\":%f,\"latitude\":%f}\r\n"
 #define AT_CMD_PUB_MQTT "AT+SMPUB=\"messages/" DEVICE_ID "/update\",%d,0,1\r\n"
 
-static char at_cmd[][CMD_SIZE] = {
-    "ATE0\r\n",
-    "AT+CENG?\r\n",
-    "AT+CNACT=0,1\r\n",
-    "AT+SMCONF=\"URL\",\"" MQTT_SERVER "\"," PORT "\r\n",
-    "AT+SMCONF=\"CLIENTID\",\"" CLIENT_ID "\"\r\n",
-    "AT+SMCONF=\"USERNAME\",\"" USERNAME "\"\r\n",
-    "AT+SMCONF=\"PASSWORD\",\"" PASSWORD "\"\r\n",
-    "AT+SMCONN\r\n",
-    "public_msg",
-    "public_data",
-    "AT+CPOWD=1\r\n",
-};
-
-static int delay_before_read[] = {
-    2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000,
-};
-
-static char response_success[][CMD_SIZE] = {
-    "E0",     "CENG?",  "CNACT",    "SMCONF", "SMCONF", "SMCONF",
-    "SMCONF", "SMCONN", "AT+SMPUB", "",       "AT",
-};
-
 static EventGroupHandle_t xEventGroup;
 static EventBits_t uxBits;
 static const char *LOG_AT_CMD = "SEND_AT_CMD";
-static int tmp, cell = 0, pci = 0, rsrp = 0, rsrq = 0, sinr = 0, cellid = 0;
+static int tmp, cell = 0, pci = 0, rsrp = 0, rsrq = 0, sinr = 0, cellid = 1;
 static float longitude = 0, latitude = 0;
 static int current;  // Current AT_command send
+struct AT_command {
+    char command[CMD_SIZE];
+    char res_success[CMD_SIZE];
+    int delay_before_read_uart;
+};
+
+struct AT_command at_cmd[] = {
+    {"ATE0\r\n", "E0", 2000},
+    {"AT+CENG?\r\n", "CENG?", 3000},
+    {"AT+CNACT=0,1\r\n", "CNACT", 3000},
+    {"AT+SMCONF=\"URL\",\"" MQTT_SERVER "\"," PORT "\r\n", "SMCONF", 500},
+    {"AT+SMCONF=\"CLIENTID\",\"" CLIENT_ID "\"\r\n", "SMCONF", 500},
+    {"AT+SMCONF=\"USERNAME\",\"" USERNAME "\"\r\n", "SMCONF", 500},
+    {"AT+SMCONF=\"PASSWORD\",\"" PASSWORD "\"\r\n", "SMCONF", 500},
+    {"AT+SMCONN\r\n", "SMCONN", 2000},
+    {"public_msg\r\n", "SMPUB", 2000},
+    {"public_data\r\n", "", 2000},
+    {"AT+CPOWD=1\r\n", "CPOWD", 5000},
+};
 
 void data_CENG_handler(const char *data) {
     char str[CMD_SIZE];
@@ -67,11 +64,13 @@ void data_CENG_handler(const char *data) {
     char public_mess[CMD_SIZE];
     snprintf(public_mess, CMD_SIZE, AT_CMD_PUB_MQTT, strlen(public_data));
 
-    memcpy(at_cmd[8], public_mess, strlen(public_mess));
-    memcpy(at_cmd[9], public_data, strlen(public_data));
+    memcpy(at_cmd[8].command, public_mess, strlen(public_mess));
+    memcpy(at_cmd[9].command, public_data, strlen(public_data));
     if (strstr(public_data, "public_data") == NULL) {
         xEventGroupSetBits(xEventGroup, AT_CMD_SUCCESS_BIT);
         ESP_LOGI("Set_CENG_data", "SUCCESS");
+    } else {
+        xEventGroupSetBits(xEventGroup, AT_CMD_FAILURE_BIT);
     }
 }
 
@@ -79,8 +78,10 @@ void data_uart_handler(char *data, int len) {
     ESP_LOGI("Receive_data", "%s", data);
     if (current == 1)
         data_CENG_handler(data);
-    else if (strstr(data, response_success[current]))
+    else if (strstr(data, at_cmd[current].res_success))
         xEventGroupSetBits(xEventGroup, AT_CMD_SUCCESS_BIT);
+    else
+        xEventGroupSetBits(xEventGroup, AT_CMD_FAILURE_BIT);
 }
 
 void powerOnSetup() {
@@ -104,9 +105,9 @@ bool send_at_handler(char *command, int current_at_cmd, int resend_count,
     while (current == current_at_cmd && count > 0) {
         ESP_LOGI("Send_command", "%s", command);
         uart_write_data(command);
-        uxBits = xEventGroupWaitBits(xEventGroup, AT_CMD_SUCCESS_BIT, pdTRUE,
-                                     pdFALSE,
-                                     pdMS_TO_TICKS(delay_before_read + 1000));
+        uxBits = xEventGroupWaitBits(
+            xEventGroup, AT_CMD_SUCCESS_BIT | AT_CMD_FAILURE_BIT, pdTRUE,
+            pdFALSE, pdMS_TO_TICKS(delay_before_read + 1000));
         if (uxBits & AT_CMD_SUCCESS_BIT) {
             current++;
         } else {
@@ -127,14 +128,14 @@ void app_main(void) {
     bool isSuccess;
     while (1) {
         powerOn();
-        for (current = 0; current < at_cmd_array_len; current++) {
-            isSuccess = send_at_handler(at_cmd[current], current, 5,
-                                        delay_before_read[current]);
+        for (current = 0; current < at_cmd_array_len - 1; current++) {
+            isSuccess = send_at_handler(at_cmd[current].command, current, 5,
+                                        at_cmd[current].delay_before_read_uart);
             if (!isSuccess) break;
         }
         current = at_cmd_array_len - 1;
-        isSuccess = send_at_handler(at_cmd[current], current, 5,
-                                    delay_before_read[current]);
+        isSuccess = send_at_handler(at_cmd[current].command, current, 5,
+                                    at_cmd[current].delay_before_read_uart);
         if (isSuccess) {
             ESP_LOGI(LOG_AT_CMD, "Power off");
         } else {
